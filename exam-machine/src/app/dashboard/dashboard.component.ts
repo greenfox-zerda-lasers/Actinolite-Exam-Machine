79773181622457317f807e471d65bfa19d3a3ae6
Olivia Gardiner <oliv.gardiner@gmail.com>
2017-01-19 17:37:58 +0100

    only login component

--- exam-machine/src/app/dashboard/dashboard.component.ts
+++ exam-machine/src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
+  // moduleId: module.id,
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  logOut() {
+    console.log('logout success');
+    this.router.navigateByUrl('/');
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
--- exam-machine/src/app/login/login.component.ts
+++ exam-machine/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from '../data.service';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-login',
@@ -13,36 +14,63 @@ import { DataService } from '../data.service';
 
 export class LoginComponent implements OnInit {
 
-  users = [];
+  // user = [];
 
   checkUser(loginUser: string, loginPass: string) {
-    let temp = this.users.slice(1);
-    console.log(temp);
-    // console.log(this.users);
-    let result = false;
-    // let user;
-    temp.forEach(function(e) {
-      // console.log(e);
-      if (e.username === loginUser) {
-        if (e.password === loginPass) {
-          result = true;
-          // user = e;
-        }
-      }
-    })
-    if (result) {
-      console.log('login success');
-      this.router.navigateByUrl('/dashboard');
-    } else {
-      console.log('login error')
-    }
+    var user;
+    this.dataService.fetchData(loginUser, loginPass)
+      .toPromise()
+      .then((data) => user = data)
+      .then(() => this.navigate())
+        // function() {
+        // console.log(user)
+        // if (user.status === 'ok') {
+        //   console.log('login success');
+        //   this.router.navigateByUrl('/dashboard');
+        // } else {
+        //   console.log('login error');
+        // }
+      // })
+      .catch(this.handleError);
   }
+
+  navigate() {
+    this.router.navigateByUrl('/dashboard');
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occurred', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
+
+  // checkUser(loginUser: string, loginPass: string) {
+  //   let temp = this.users.slice(1);
+  //   console.log(temp);
+  //   // console.log(this.users);
+  //   let result = false;
+  //   let user;
+  //   temp.forEach(function(e) {
+  //     // console.log(e);
+  //     if (e.email === loginUser) {
+  //       if (e.password === loginPass) {
+  //         result = true;
+  //         user = e;
+  //       }
+  //     }
+  //   })
+  //   if (result) {
+  //     console.log('login success');
+  //     this.router.navigateByUrl('/dashboard');
+  //   } else {
+  //     console.log('login error')
+  //   }
+  // }
 
   constructor(private dataService: DataService, private router: Router) { }
 
   ngOnInit() {
-    this.dataService.fetchData().subscribe(
-      (data) => this.users = data,
-    );
+    // this.dataService.fetchData().subscribe(
+    //   (data) => this.users = data
+    // );
   }
 }
