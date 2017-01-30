a5e8383cef8fd96b164d67ce150387fb4d179d99
Máté Szurovecz <szuri92@gmail.com>
2017-01-30 14:15:01 +0100

    add alpha view

--- exam-machine/src/app/dashboard/mentor/exams/exams.component.ts
+++ exam-machine/src/app/dashboard/mentor/exams/exams.component.ts
@@ -7,6 +7,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExamsComponent implements OnInit {
 
+  exams = [
+    {
+      id: '1',
+      name: 'JavaScript',
+      mentor: 'Envagyok Béla',
+      cohort: 'Zerda',
+      class: 'Lasers'
+    },
+    {
+      id: '2',
+      name: 'C++',
+      mentor: 'Nemvagyok Béla',
+      cohort: 'Zerda',
+      class: 'Sparta'
+    },
+    {
+      id: '3',
+      name: 'Java',
+      mentor: 'Enpedig Juli',
+      cohort: 'Zerda',
+      class: 'Raptor'
+    }
+  ];
+
+  cohorts = [
+    {
+      id: '1',
+      name: 'Zerda'
+    },
+    {
+      id: '2',
+      name: 'Velox'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
