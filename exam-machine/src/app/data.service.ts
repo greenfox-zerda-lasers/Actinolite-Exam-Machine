13f244c753149a94965937b66bf7b5bfce2a44ed
Olivia Gardiner <oliv.gardiner@gmail.com>
2017-02-03 11:07:05 +0100

    Frontend requests written

--- exam-machine/src/app/data.service.ts
+++ exam-machine/src/app/data.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Headers } from '@angular/http';
 
 @Injectable()
 
@@ -7,18 +7,80 @@ export class DataService {
 
   constructor(private http: Http) { }
 
+  private headers = new Headers({'Content-Type': 'application/json'});
+
   userLogin(email, password) {
-    return this.http.post('https://exam-machine-backend.gomix.me/user/login', {email: email, password: password})
+    return this.http.post('https://exam-machine-backend.gomix.me/user/login', {email: email, password: password}, {headers: this.headers})
       .map((res) => res.json());
   }
 
-  // fetchData(email) {
-  //   return this.http.post('https://exam-machine-backend.gomix.me', {email: email})
-  //     .map((res) => res.json());
-  // }
-
   userSignup(name, email, password) {
-    return this.http.post('https://exam-machine-backend.gomix.me/user/signup', {name: name, email: email, password: password})
+    return this.http.post('https://exam-machine-backend.gomix.me/user/signup', {user_name: name, user_email: email, user_password: password}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  fetchClasses() {
+    return this.http.get('https://exam-machine-backend.gomix.me/dashboard/classes')
+      .map((res) => res.json())
+  }
+
+  addNewClass(name) {
+    return this.http.post('https://exam-machine-backend.gomix.me/dashboard/classes', {class_name: name}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  assignClassToCohort(name, id) {
+    return this.http.put('https://exam-machine-backend.gomix.me/dashboard/classes-to-cohorts', {class_name: name, cohort_id: id}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  editClass(name) {
+    return this.http.put('https://exam-machine-backend.gomix.me/dashboard/classes', {class_name: name}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  fetchCohorts() {
+    return this.http.get('https://exam-machine-backend.gomix.me/dashboard/cohorts')
+      .map((res) => res.json())
+  }
+
+  addNewCohort(name) {
+    return this.http.post('https://exam-machine-backend.gomix.me/dashboard/cohorts', {cohort_name: name}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  editCohort(name) {
+    return this.http.put('https://exam-machine-backend.gomix.me/dashboard/cohorts', {cohort_name: name}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  fetchStudents() {
+    return this.http.get('https://exam-machine-backend.gomix.me/dashboard/students')
+      .map((res) => res.json())
+  }
+
+  fetchUnassigned() {
+    return this.http.get('https://exam-machine-backend.gomix.me/dashboard/unassigned')
+      .map((res) => res.json())
+  }
+
+  assignStudentToClass(studentid, classid) {
+    return this.http.post('https://exam-machine-backend.gomix.me/dashboard/students', {user_id: studentid, class_id: classid}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  fetchExams() {
+    return this.http.get('https://exam-machine-backend.gomix.me/dashboard/exams')
+      .map((res) => res.json())
+  }
+
+  addNewExam(name, description, repo, duration, classid, userid) {
+    return this.http.post('https://exam-machine-backend.gomix.me/dashboard/exams', {exam_name: name, exam_desc: description, exam_duration: duration, exam_repo: repo, exam_creator_id: userid, exam_class: classid}, {headers: this.headers})
+      .map((res) => res.json())
+  }
+
+  changeExamState(state) {
+    return this.http.put('https://exam-machine-backend.gomix.me/dashboard/exams', {exam_status: state}, {headers: this.headers})
       .map((res) => res.json())
   }
 
